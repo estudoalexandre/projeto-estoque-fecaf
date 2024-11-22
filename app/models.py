5a04cc086a7751468079b6717065b5a7dc766e4f
Alexandre Oliveira <alexandresousaa70@gmail.com>
2024-11-22 19:43:08 -0300

    Estrutura configurada para iniciar o projeto com models pronta..

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from app import db
+from flask_login import UserMixin
+
+
+class Usuario(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    nivel_funcao = db.Column(db.String(15), nullable=False, default='comum') 
+
+class Produto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(150), nullable=False)
+    quantidade = db.Column(db.Integer, nullable=False)
+    minimo_estoque = db.Column(db.Integer, nullable=False)
+    preco_unitario = db.Column(db.Float, nullable=False)
